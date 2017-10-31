f3206fa120bf9539657365d59d55fcbf74d6f779
BRANDONHP\brand <brandon.urias@my.simpson.edu>
2017-10-31 10:09:01 -0500

    finale

--- public.html/page_3.php
+++ public.html/page_3.php
@@ -16,36 +16,7 @@
 <body>
   <!-- start nav bar-->
 
-     <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-
-            <!-- Three line "hamburger" icon for the collapsed navigation -->
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <!-- Title on nav bar -->
-          <a class="navbar-brand" href='index.html'>Home</a>
-        </div>
-
-        <div id="navbar" class="collapse navbar-collapse">
-         <ul class="nav navbar-nav">        
-           <li><a href='login.html'>Login</a></li>
-           <li><a href='sign_up.html'>Sign Up Here</a></li>
-           <li><a href='page_2.html'>Menu</a></li>
-           <li><a class = "active" href='page_3.html'>About/Contact Us</a></li>
-           <li><a href='page_4.html'>Events</a></li>
-           <li><a href='page_5.html'>Desserts</a></li>
-           <li><a href='page_6.html'>Locations</a></li>
-           <li><a href='page_7.html'>Drinks</a></li>
-         </ul>
-        </div>
-      </div>
-     </nav>
+        <?php include '../resources/navigation.php'; ?>
  <!-- end nav bar-->
     
 <div class="below_menu_container">
@@ -88,9 +59,7 @@
  
 </div>
 
- <footer> 
-     <a class="footer" href='http://www.blr.com/HR-Employment/HR-Administration/Privacy-in-Iowa#'>&copy;       Privacy Notice (Updated September 4, 2017)/ Your Iowa Privacy Rights </a>
- </footer>
+      <?php include '../resources/footer.php'; ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
